0c639901623587d01473660e2a288d33cd028bc8
Scott Addie <tobias.addie@gmail.com>
2015-06-23 20:42:42 -0500

    minor performance enhancements

--- app/app.module.js
+++ app/app.module.js
@@ -10,13 +10,17 @@ import uiRouter from 'npm/angular-ui-router';
 // custom module imports
 import {default as PersonModule} from './person/person.module';
 
-angular.module('app', [
-	// vendor modules
-	ngAria,
-	uiRouter,
-	formly,
-	formlyBootstrap,
+angular
+	.module('app', [
+		// vendor modules
+		ngAria,
+		uiRouter,
+		formly,
+		formlyBootstrap,
 		
-	// custom modules
-	PersonModule.name
-]);
+		// custom modules
+		PersonModule.name
+	])
+	.config(['$compileProvider', function ($compileProvider) {
+		$compileProvider.debugInfoEnabled(false);
+	}]);
